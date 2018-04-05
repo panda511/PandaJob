f92a1a1288fb4fca98566560fff592900df98a49
Panda <Panda>
2018-04-05 22:47:18 +0800

    a

--- PandaJob/Controllers/AuthorizationFilter.cs
+++ PandaJob/Controllers/AuthorizationFilter.cs
@@ -10,16 +10,7 @@ namespace PandaJob.Controllers
     {
         public bool Authorize(DashboardContext context)
         {
-            // In case you need an OWIN context, use the next line, `OwinContext` class
-            // is the part of the `Microsoft.Owin` package.
-            //var owinContext = new OwinContext(context.GetOwinEnvironment());
-
-            // Allow all authenticated users to see the Dashboard (potentially dangerous).
-            //return owinContext.Authentication.User.Identity.IsAuthenticated;
-
-            //todo:使用Form认证  0328
-
-            return true;
+            return HttpContext.Current.Request.IsAuthenticated;
         }
     }
 }
--- PandaJob/Controllers/HomeController.cs
+++ PandaJob/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace PandaJob.Controllers
 {
@@ -16,6 +17,27 @@ namespace PandaJob.Controllers
         public ActionResult Login() 
         {
             return View();
+        }
+
+        public JsonResult SignIn(string name, string password)
+        {
+            if (name == "admin" && password == "1234")
+            {
+                //form认证
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, name, DateTime.Now, DateTime.Now.AddDays(7), false, name);
+                string str = FormsAuthentication.Encrypt(ticket);
+                HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, str);
+                cookie.Expires = ticket.Expiration;
+                Response.Cookies.Add(cookie);
+                return Json(new { Code = 200, Message = "success" });
+            }
+
+            var r = new
+            {
+                Code = 100,
+                Message = "failure"
+            };
+            return Json(r);
         }
     }
 }
